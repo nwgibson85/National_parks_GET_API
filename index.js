1fe0ababd01e8352e93e696f11650043a74df87e
Nicholas Gibson <Nwgibson85@gmail.com>
2019-06-20 21:58:15 -0500

    IT WORKS

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 
 
 const apiKey = 'Fzu44deYrqnCFJgrh3Dg9Vd0jqlqcEckyzIz8OvG'; 
-const baseURL = 'https://www.developer.nps.gov/api/v1/parks';
+const baseURL = 'https://developer.nps.gov/api/v1/parks';
 
-const options = {
-      "X-Api-Key:": apiKey
-};
+// const options = {
+//     headers: new Headers({
+//       "X-Api-Key": apiKey})
+//   }; 
 
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
@@ -17,17 +18,19 @@ function formatQueryParams(params) {
 function displayResults(responseJson) {
   console.log(responseJson);
   $('#results-list').empty();
-  for (let i = 0; i < responseJson.items.length; i++){
+  for (let i = 0; i < responseJson.data.length; i++){
     $('#results-list').append(
       `<li><h3>${responseJson.data[i].fullName}</h3>
       <p>${responseJson.data[i].description}</p>
-      <img src='${responseJson.data[i].url}'>
-        <ul id='address'><h3>Address:</h3>
-            <li id='line1'>${response.Json.data[i].addresses.line1}</li>
-            <li id='line1'>${response.Json.data[i].addresses.line2}</li>
-            <li id='city'>${response.Json.data[i].addresses.city}, ${response.Json.data[i].addresses.statecode}, ${response.Json.data[i].addresses.postalcode}</li>
-        </ul>
-        </li>`
+      <a href='${responseJson.data[i].url}'>${responseJson.data[i].url}</a>
+      <ul id='address'><h3>Address:</h3>
+            <li id='line1'>${responseJson.data[i].addresses[0].line1}</li>
+            <li id='line2'>${responseJson.data[i].addresses[0].line2}</li>
+            <li id='city'>${responseJson.data[i].addresses[0].city}</li>
+            <li id='state'>${responseJson.data[i].addresses[0].stateCode}</li>
+            <li id='Zip'>${responseJson.data[i].addresses[0].postalCode}</li>
+            </ul>
+    </li>`
     )};
   $('#results').removeClass('hidden');
 };
@@ -36,14 +39,15 @@ function getParksByState(searchState, maxResults=10) {
   const params = {
     stateCode: searchState,
     limit: maxResults,
-    // api_key: apiKey
+    fields: "addresses",
+    api_key: apiKey
   };
   const queryString = formatQueryParams(params)
   const url = baseURL + "?" + queryString;
 
   console.log(url);
 
-  fetch(url, options)
+  fetch(url)
     .then(response => {
       if (response.ok) {
         return response.json();
@@ -65,4 +69,5 @@ function watchForm() {
   });
 }
 
-$(watchForm);
+$(watchForm); 
+
